683f4be2aebb90bdb999a8a83c6cf41187c25cee
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-20 23:40:45 -0500

    add add_task method

--- task_manager.py
+++ task_manager.py
@@ -1,2 +1,12 @@
 from task import Task
 from utils import generate_task_id, validate_date
+
+
+def add_task(tasks, title, description, due_date, priority):
+    if not validate_date(due_date):
+        raise ValueError("Invalid date format")
+
+    task_id = generate_task_id(tasks)
+    task = Task(task_id, title, description, due_date, "PENDING", priority)
+    tasks.append(task.to_dict())
+    return tasks
