a7d6c7307609735e6604e01270a448cead3ff537
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-20 23:33:10 -0500

    add constructor

--- task.py
+++ task.py
@@ -1,1 +1,8 @@
 class Task:
+    def __init__(self, task_id, title, description, due_date, status, priority):
+        self.task_id = task_id
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+        self.priority = priority
