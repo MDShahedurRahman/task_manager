606381d25dec05efcaa3de9329277ff10818e273
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-20 23:35:51 -0500

    add load_tasks method

--- task_loader.py
+++ task_loader.py
@@ -1,2 +1,10 @@
 import json
 from config import DATA_FILE
+
+
+def load_tasks():
+    try:
+        with open(DATA_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
