8be045d61fce037475e779e9293c5c4256522a12
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-21 23:27:11 -0500

    load tasks on startup

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from task_loader import load_tasks, save_tasks
 from task_manager import add_task
 from task_analyzer import count_by_status
 from report import summary_report, print_report
+
+
+def main():
+    tasks = load_tasks()
+
+
+if __name__ == "__main__":
+    main()
