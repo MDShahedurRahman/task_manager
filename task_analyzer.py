cd4f44e857a8c04b2ff1a941790128bd6b94fc34
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-21 23:21:16 -0500

    add high priority filter

--- task_analyzer.py
+++ task_analyzer.py
@@ -3,3 +3,7 @@ from utils import validate_date
 
 def filter_by_status(tasks, status):
     return [t for t in tasks if t["status"] == status]
+
+
+def filter_by_due_date(tasks, due_date):
+    return [t for t in tasks if t["due_date"] == due_date]
