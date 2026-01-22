5604239fa180265aa41ee91593c6e5386183a31b
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-21 23:21:55 -0500

    add overdue task detection

--- task_analyzer.py
+++ task_analyzer.py
@@ -6,4 +6,6 @@ def filter_by_status(tasks, status):
 
 
 def filter_by_due_date(tasks, due_date):
+    if not validate_date(due_date):
+        return []
     return [t for t in tasks if t["due_date"] == due_date]
